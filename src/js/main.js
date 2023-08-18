f82b92f5c04f82df29b97a9f9209cb98ebcceb34
EgorForAll <eia.web.ss@gmail.com>
2023-08-18 19:44:53 +0300

    wordpress integrated, contact form 7 integrated, acf is executed

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".wpcf7-form");
+
+form.addEventListener(
+  "submit",
+  () => {
+    console.log("Кто-то отправил форму! Какую именно? Пока не знаю.");
+  },
+  false
+);
